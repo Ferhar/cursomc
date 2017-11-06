70e94132e7c9fcf13a253dc65e0abbd829755c9c
fernandohar <fernando.henrique_93@hotmail.com>
2017-11-06 21:03:33 -0200

    Implementando toString de Pedido

--- src/main/java/com/fernandohar/cursomc/services/PedidoService.java
+++ src/main/java/com/fernandohar/cursomc/services/PedidoService.java
@@ -9,6 +9,7 @@ import com.fernandohar.cursomc.domain.ItemPedido;
 import com.fernandohar.cursomc.domain.PagamentoComBoleto;
 import com.fernandohar.cursomc.domain.Pedido;
 import com.fernandohar.cursomc.domain.enums.EstadoPagamento;
+import com.fernandohar.cursomc.repositories.ClienteRepository;
 import com.fernandohar.cursomc.repositories.ItemPedidoRepository;
 import com.fernandohar.cursomc.repositories.PagamentoRepository;
 import com.fernandohar.cursomc.repositories.PedidoRepository;
@@ -32,6 +33,9 @@ public class PedidoService {
 	 
 	 @Autowired
 	 private ItemPedidoRepository itemPedidoRepository;
+	 
+	 @Autowired
+	 private ClienteRepository clienteRepository;
 	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
@@ -50,6 +54,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		 obj.setId(null);
 		 obj.setInstante(new Date());
+		 obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		 obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		 obj.getPagamento().setPedido(obj);
 		 if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -61,9 +66,12 @@ public class PedidoService {
 		 	for (ItemPedido ip : obj.getItens()) {
 		 		ip.setDesconto(0.0);
 		 		ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+		 		ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+		 		ip.setPreco(ip.getProduto().getPreco());
 		 		ip.setPedido(obj);
 		 	}
 		 	itemPedidoRepository.save(obj.getItens());
+		 	System.out.println(obj);
 		 	return obj;
 		 }
 
