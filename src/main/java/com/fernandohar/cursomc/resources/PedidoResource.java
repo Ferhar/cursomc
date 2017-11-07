277a08c31017c861b61a44de3a01dec92b078c4e
fernandohar <fernando.henrique_93@hotmail.com>
2017-11-07 18:31:54 -0200

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/fernandohar/cursomc/resources/PedidoResource.java
+++ src/main/java/com/fernandohar/cursomc/resources/PedidoResource.java
@@ -3,11 +3,13 @@ package com.fernandohar.cursomc.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -15,34 +17,42 @@ import com.fernandohar.cursomc.domain.Pedido;
 import com.fernandohar.cursomc.services.PedidoService;
 
 @RestController
-@RequestMapping(value="/pedidos")
+@RequestMapping(value = "/pedidos")
 public class PedidoResource {
-	
+
 	@Autowired
 	private PedidoService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
+
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> inserir(@RequestBody Pedido obj) {
-		
+
 		service.insert(obj);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
-				
-				return ResponseEntity.created(uri).build();
-		
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Pedido obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/fernandohar/cursomc/services/PedidoService.java
+++ src/main/java/com/fernandohar/cursomc/services/PedidoService.java
@@ -3,8 +3,12 @@ package com.fernandohar.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.fernandohar.cursomc.domain.Cliente;
 import com.fernandohar.cursomc.domain.ItemPedido;
 import com.fernandohar.cursomc.domain.PagamentoComBoleto;
 import com.fernandohar.cursomc.domain.Pedido;
@@ -14,68 +18,78 @@ import com.fernandohar.cursomc.repositories.ItemPedidoRepository;
 import com.fernandohar.cursomc.repositories.PagamentoRepository;
 import com.fernandohar.cursomc.repositories.PedidoRepository;
 import com.fernandohar.cursomc.repositories.ProdutoRepository;
+import com.fernandohar.cursomc.security.UserSS;
+import com.fernandohar.cursomc.services.exceptions.AuthorizationException;
 import com.fernandohar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
-	 private BoletoService boletoService;
-	 	
-	 @Autowired
-	 private PagamentoRepository pagamentoRepository;
-	 
-	 @Autowired
-	 private ProdutoRepository produtoRepository;
-	 
-	 @Autowired
-	 private ItemPedidoRepository itemPedidoRepository;
-	 
-	 @Autowired
-	 private ClienteRepository clienteRepository;
-	 
-	 @Autowired
-	 private EmailService emailService;
-	
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EmailService emailService;
+
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Pedido.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
 		}
 		return obj;
 	}
-	
+
 	public Pedido update(Pedido obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
-	public Pedido insert(Pedido obj) {
-		 obj.setId(null);
-		 obj.setInstante(new Date());
-		 obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
-		 obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
-		 obj.getPagamento().setPedido(obj);
-		 if (obj.getPagamento() instanceof PagamentoComBoleto) {
-			 PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-		 	boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
-		 }
-		 	obj = repo.save(obj);
-		 	pagamentoRepository.save(obj.getPagamento());
-		 	for (ItemPedido ip : obj.getItens()) {
-		 		ip.setDesconto(0.0);
-		 		ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
-		 		ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
-		 		ip.setPreco(ip.getProduto().getPreco());
-		 		ip.setPedido(obj);
-		 	}
-		 	itemPedidoRepository.save(obj.getItens());
-		 	emailService.sendOrderConfirmationEmail(obj);
-		 	return obj;
-		 }
 
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.save(obj.getItens());
+		emailService.sendOrderConfirmationEmail(obj);
+		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
